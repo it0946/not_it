5f9d0da6d68728d4c4d70d2a6ea4a77d79a93935
it0946 <it000946@gmail.com>
2021-03-08 18:08:55 +0200

    Changed a couple things in config.py

--- util/config.py
+++ util/config.py
@@ -5,6 +5,10 @@ import json
 config = json.load(open('config.json', 'r'))
 respond_to = config['id']
 
+async def saveConfig(newConfig: dict):
+    with open('config.json', 'w') as file:
+        file.write(json.dumps(newConfig))
+
 async def id_exists(id: int): # Check if the id exists in the array
         for _id in respond_to:
             if id == _id: 
@@ -14,18 +18,15 @@ async def addid(id: int): # Add an ID to the array
     if not await id_exists(id):
         newConfig = config
         newConfig['id'].append(id)
-        with open('config.json', 'w') as file:
-            file.write(json.dumps(newConfig))
+        await saveConfig(newConfig)
 
 async def delid(id: int): # Remove an ID from the array
     if await id_exists(id):
         newConfig = config
-        index = await indexof(id)
-        newConfig['id'].pop(index)
-        with open('config.json', 'w') as file:
-            file.write(json.dumps(newConfig))
+        newConfig['id'].pop(await indexof(id, 'id'))
+        await saveConfig(newConfig)
 
-async def indexof(id: int): # Get the index of an ID for delid()
-    for i in range(0, len(config['id'])):
-        if config['id'][i] == id:
+async def indexof(id: int, arg: str): # Get the index of an ID for delid()
+    for i in range(0, len(config[arg])):
+        if config[arg][i] == id:
             return i
