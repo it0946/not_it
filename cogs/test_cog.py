215aec4068dcf1d21fd0a74ca8a188ed966c1123
it0946 <it000946@gmail.com>
2021-03-08 21:53:30 +0200

    Added a command to change the bot prefix

--- cogs/test_cog.py
+++ cogs/test_cog.py
@@ -1,9 +1,23 @@
 from discord.ext import commands
-from discord.ext.commands.errors import CommandNotFound
+from util.config import change_prefix
+import json
+
+config = json.load(open('config.json', 'r'))
 
 class test_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def prefix(self, ctx, prefix: str = None):
+        if prefix is None:
+            await ctx.send('The current prefix is: `{0}`'.format(config['prefix']))
+        else:
+            if not len(prefix) >= 4: 
+                await change_prefix(prefix)
+                self.bot.command_prefix = config['prefix'] = prefix
+            else: 
+                await ctx.send('Requested prefix is too long. Limit is 4 characters')
 
     @commands.command(name='test')
     async def test(self, ctx):
--- util/config.py
+++ util/config.py
@@ -1,4 +1,5 @@
 import json
+#from main import bot # breaks this apparently?
 
 # Misc config functions to do with config.json  
 
@@ -20,13 +21,18 @@ async def addid(id: int): # Add an ID to the array
         newConfig['id'].append(id)
         await saveConfig(newConfig)
 
+async def change_prefix(newPrefix: str):
+    newConfig = config
+    newConfig['prefix'] = newPrefix
+    await saveConfig(newConfig)
+
 async def delid(id: int): # Remove an ID from the array
     if await id_exists(id):
         newConfig = config
         newConfig['id'].pop(await indexof(id, 'id'))
         await saveConfig(newConfig)
 
-async def indexof(id: int, arg: str): # Get the index of an ID for delid()
+async def indexof(id: int, arg: str): # Get the index of an id in the config
     for i in range(0, len(config[arg])):
         if config[arg][i] == id:
             return i
