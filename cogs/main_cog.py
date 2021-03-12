1a712b5008138420a0f9f57f5dd86abad6079750
it0946 <it000946@gmail.com>
2021-03-12 14:57:07 +0200

    Changing things for classes, incomplete

--- cogs/main_cog.py
+++ cogs/main_cog.py
@@ -1,27 +1,31 @@
-from logging import NullHandler
 from discord.ext import commands
-from util.servers import change_prefix, get_guild_prefix, get_prefix
+# from util.servers import change_prefix, get_guild_prefix, get_prefix
+from util.Servers import Servers
 import json
 
 class main_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.servers = Servers
 
     # TODO: Figure out how to properly do permissions for commands
     @commands.command()
     async def prefix(self, ctx, prefix: str = None):
-        server_config = json.load(open('servers.json', 'r'))
-        _prefix = await get_guild_prefix(ctx.guild.id)
+        servers = self.servers
+        if ctx.guild is None:
+            await ctx.send(f'The prefix is {servers._get_prefix(ctx.message)}')
+            return
+        guildPrefix = await servers.get_guild_prefix(ctx.guild.id)
         if prefix is None:
-            await ctx.send(f'The current prefix is: `{prefix}`, or you can ping the bot.')
+            await ctx.send(f'The current prefix is: `{guildPrefix}`, or you can ping the bot.')
         else:
             if not len(prefix) > 4:
-                if prefix == _prefix:
-                     await ctx.send('New prefix cannot be the old prefix.')
-                     return
-                await change_prefix(ctx.guild.id, prefix)
+                if prefix == guildPrefix:
+                    await ctx.send('New prefix cannot be the old prefix.')
+                    return
+                await servers.change_prefix(ctx.guild.id, prefix)
                 await ctx.send(f'Success: The new bot prefix is `{prefix}`')
-            else: 
+            else:
                 await ctx.send('Requested prefix is too long. Limit is 4 characters')
 
     @commands.command(name='test')
--- not_it.py
+++ not_it.py
@@ -1,13 +1,17 @@
-from os import pread
-from sys import prefix
-from discord.ext import commands
-from discord.flags import Intents
 import discord
-import json
+from discord.ext import commands
+from util.config import Config
+from util.servers import Servers
 
-from util.servers import get_prefix
+config = Config
+servers = Servers
 
-# WIP
+async def get_prefix(bot, message: discord.Message):
+    prefixes = [f'<@{bot.user.id}> ', f'<@!{bot.user.id}> ']
+    if message.guild is None:
+        prefix = await config.default_prefix
+    else:
+        prefix = await servers.
 
 class not_it(commands.Bot):
     def __init__(self):
@@ -20,6 +24,35 @@ class not_it(commands.Bot):
             reactions = True
         )
 
-        super().__init__(command_prefix=prefix, intents=intents)
-        
-    
+        super().__init__(
+            intents=intents,
+            command_prefix = get_prefix,
+            description= 'Discord bot for learning and messing around'
+            )
+
+    async def process_commands(self, message):
+        return super().process_commands(message)
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            return
+        elif isinstance(error, commands.ArgumentParsingError):
+            await ctx.send(f'')
+
+    async def log(self):
+        pass 
+
+    async def run(self, *args, **kwargs):
+        return super().run(*args, **kwargs)
+
+
+    '''
+    some sort of logging
+    on_ready # No clue
+    process_commands
+    on_guild_join
+    on_message
+    run
+    close
+    spam_control
+    '''
